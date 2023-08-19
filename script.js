81024925537d955b7dc04537ee5a3fb51e217984
duncan <134294874+d-duncan-n@users.noreply.github.com>
2023-08-19 17:51:05 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+const board = document.getElementById('game-board');
+const size = 4;
+let tiles = [];
+let emptyTile = { row: size - 1, col: size - 1 };
+
+function createTile(value) {
+    const tile = document.createElement('div');
+    tile.classList.add('tile');
+    tile.textContent = value === size * size ? '' : value;
+    return tile;
+}
+
+function shuffleTiles() {
+    tiles = [];
+    for (let i = 1; i <= size * size; i++) {
+        tiles.push(i);
+    }
+    for (let i = tiles.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [tiles[i], tiles[j]] = [tiles[j], tiles[i]];
+    }
+}
+
+function renderBoard() {
+    board.innerHTML = '';
+    for (let row = 0; row < size; row++) {
+        for (let col = 0; col < size; col++) {
+            const value = tiles[row * size + col];
+            const tile = createTile(value);
+            tile.addEventListener('click', () => moveTile(row, col));
+            board.appendChild(tile);
+        }
+    }
+}
+
+function moveTile(row, col) {
+    if (Math.abs(emptyTile.row - row) + Math.abs(emptyTile.col - col) !== 1) {
+        return;
+    }
+    tiles[emptyTile.row * size + emptyTile.col] = tiles[row * size + col];
+    tiles[row * size + col] = size * size;
+    emptyTile = { row, col };
+    renderBoard();
+    if (checkWin()) {
+        setTimeout(() => alert('Вы победили!'), 100);
+    }
+}
+
+function checkWin() {
+    for (let i = 0; i < size * size - 1; i++) {
+        if (tiles[i] !== i + 1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function init() {
+    shuffleTiles();
+    renderBoard();
+}
+
+init();
